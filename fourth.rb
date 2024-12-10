1487faae7765a3962fdf310509a28b2eec388bfb
Abhinavlather2004 <latherabhinav55@gmail.com>
2024-12-10 15:13:30 +0530

    [Abhinav] MOD: Changes done

--- fourth.rb
+++ fourth.rb
@@ -1,5 +1,21 @@
 #Write a program that simulates a simple counter. It attempts to divide a number by a given divisor. If the divisor is 0, retry the division after prompting the user for a new divisor.
+
+#with using retry statement
+begin 
+  print "Enter a divisor: "
+  n = gets.chomp.to_i
+  raise "error!!" if n == 0
+  puts "#{n} is a valid divisor. "
+rescue
+  puts "Invalid divisor. Input another divisor. "
+  retry if n == 0
+end
+
+
+
 =begin
+
+#With using redo statement
 loop do
   print "Enter a divisor: "
   n = gets.chomp.to_i
@@ -12,14 +28,3 @@ loop do
   end
 end
 =end
-
-begin 
-  print "Enter a divisor: "
-  n = gets.chomp.to_i
-  raise "error!!" if n == 0
-  puts "#{n} is a valid divisor. "
-rescue
-  puts "Invalid divisor. Input another divisor. "
-  retry if n == 0
-end
-
