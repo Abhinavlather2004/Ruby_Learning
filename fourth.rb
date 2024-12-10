04e123e2bbab51cadd1db1c93be83bc2e8bf1d35
Abhinavlather2004 <latherabhinav55@gmail.com>
2024-12-10 14:55:58 +0530

    [Abhinav]ADD: Checking if the divisor is valid or not

---
+++ fourth.rb
@@ -0,0 +1,25 @@
+#Write a program that simulates a simple counter. It attempts to divide a number by a given divisor. If the divisor is 0, retry the division after prompting the user for a new divisor.
+=begin
+loop do
+  print "Enter a divisor: "
+  n = gets.chomp.to_i
+  if n != 0
+    puts "It is a valid divisor"
+    break
+  else
+    puts "Not a valid divisor. Retry with new divisor!"
+    redo
+  end
+end
+=end
+
+begin 
+  print "Enter a divisor: "
+  n = gets.chomp.to_i
+  raise "error!!" if n == 0
+  puts "#{n} is a valid divisor. "
+rescue
+  puts "Invalid divisor. Input another divisor. "
+  retry if n == 0
+end
+
