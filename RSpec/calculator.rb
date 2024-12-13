8a499afccc84ac78caaf08c2550a88ce9e373a07
Abhinavlather2004 <latherabhinav55@gmail.com>
2024-12-13 10:55:37 +0530

    [Abhinav]ADD: testing of calculator

---
+++ RSpec/calculator.rb
@@ -0,0 +1,20 @@
+class Calculator
+  def add(a, b)
+    a + b
+  end
+  def subtract(a, b)
+    a - b
+  end
+  def multiply(a, b)
+    a * b
+  end
+  def divide(a, b)
+    begin
+      a/b
+    rescue ZeroDivisionError => e 
+      puts e.message
+    end
+  
+  end
+end
+
